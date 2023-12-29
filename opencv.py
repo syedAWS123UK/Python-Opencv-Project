e7b3ac1868d93247dadbffc4b3df779d9ad45f20
Syed Fareed Ahmed <syedahmed261@gmail.com>
2023-12-29 05:41:02 +0530

    Update opencv.py

--- opencv.py
+++ opencv.py
@@ -1,20 +1,31 @@
 import cv2
-import numbpy as np
+import numpy as np
+import sys
 
-img = cv2.imread('C:\Users\Computer\Picture\Highway.jpg' ,0)
+if len(sys.argv) < 2:
+    print("Usage: python opencv.py <image_path>")
+    sys.exit(1)
+
+image_path = sys.argv[1]
+img = cv2.imread(image_path)
+
+if img is None:
+    print(f"Error: Unable to read image from {image_path}")
+    sys.exit(1)
+
 width = 600
 height = 850
-dim = (width,height)
+dim = (width, height)
 resized = cv2.resize(img, dim)
 
-kernel = np.ones((5,5), dtype='vinbt8')
+kernel = np.ones((5, 5), dtype='uint8')
 
-opening = cv2.morphologyEx(resized,cv2.MORPH_OPEN,kernel)
-closing = cv2.morphologyEX(resized,cv2.MORPH_CLOSE,kernel)
+opening = cv2.morphologyEx(resized, cv2.MORPH_OPEN, kernel)
+closing = cv2.morphologyEx(resized, cv2.MORPH_CLOSE, kernel)
 
 cv2.imshow("Original", resized)
-cv2.imshow("opening", opening)
+cv2.imshow("Opening", opening)
 cv2.imshow("Closing", closing)
 
-cv2.waitkey(0)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
